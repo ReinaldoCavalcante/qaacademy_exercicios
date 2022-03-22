c47438aa104d157a15f36c93c5de32f758e93e13
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-03-21 21:08:49 -0300

    ExEasy02 Refatoracao

---
+++ src/easy/ExecucaoEx02.java
@@ -0,0 +1,9 @@
+package easy;
+
+public class ExecucaoEx02 {
+    public static void main(String[] args) {
+    Exercicio02 execucao02 = new Exercicio02();
+    System.out.println(execucao02.retornaPalavraDigitada("Teste Palavra"));
+
+    }
+}
--- src/easy/Exercicio02.java
+++ src/easy/Exercicio02.java
@@ -1,11 +1,8 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio02 {
-  public static void main(String[] args) {
-   String palavraDigitada;  
-   palavraDigitada=JOptionPane.showInputDialog("Digite uma Palavra!");
-   System.out.println("Você digitou:"+palavraDigitada);
+  
+   public String retornaPalavraDigitada (String  palavraDigitada){
+   return "você digitou:" + palavraDigitada;
   }  
 }
