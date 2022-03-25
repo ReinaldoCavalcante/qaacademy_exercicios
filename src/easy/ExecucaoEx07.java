fc2539b19f2487b7ae78de9780dee8e8a10469e4
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-03-25 20:35:50 -0300

    ExEasy07 Refatoracao

---
+++ src/easy/ExecucaoEx07.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class ExecucaoEx07 {
+    public static void main(String[] args) {
+        
+     Exercicio07 execucao07 = new Exercicio07();
+     
+     
+    System.out.println(execucao07.calculaInss(1.000));
+    
+    }
+}
