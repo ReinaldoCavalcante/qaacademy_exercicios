0ab44b5e8d1e3f7504d52bc8975cdde67d53ea35
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-03-30 20:55:09 -0300

    ExEasy08 Refatoracao

---
+++ src/easy/Execucao06.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class Execucao06 {
+    public static void main(String[] args) {
+        
+     Exercicio06 e = new Exercicio06();
+      
+    
+     System.out.println( e.calcularNota(8 , 9));
+
+    }
+}
---
+++ src/easy/ExecucaoEx08.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class ExecucaoEx08 {
+    public static void main(String[] args) {
+        
+     Exercicio08 execucao08 = new Exercicio08();
+
+     System.out.println(execucao08.calcularImposto(3.000));
+
+
+    }
+}
