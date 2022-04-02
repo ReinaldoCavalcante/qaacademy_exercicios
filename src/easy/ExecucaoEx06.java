a046cf631d0a18aa63486aad23f4b9c9b7deb145
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-04-02 11:57:15 -0300

    Alteraçao Ex.06

---
+++ src/easy/ExecucaoEx06.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class Execucao06 {
+    public static void main(String[] args) {
+        
+     Exercicio06  execucao06 = new Exercicio06();
+      
+    
+     System.out.println(execucao06.verificaAprovacao(8, 10));
+
+    }
+}
