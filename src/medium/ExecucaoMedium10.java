840087136e36a1bf97fd590b4e9a51daca6a5d0b
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-04-09 13:22:37 -0300

    Alteracoes ExMedium10

---
+++ src/medium/ExecucaoMedium10.java
@@ -0,0 +1,13 @@
+package medium;
+
+public class ExecucaoMedium10 {
+    public static void main(String[] args) {
+        
+        Exercicio10 exercicio10 = new Exercicio10();
+        System.out.println("Exibe o valor do termo: " + exercicio10.calcularFibonacci(10));
+        exibirVetor(exercicio10.calcularFibonacciVetor(10));
+    }
+
+    public static void exibirVetor(int[] calcularFibonacciVetor) {
+    }
+}
--- src/medium/Exercicio10.java
+++ src/medium/Exercicio10.java
@@ -1,31 +1,32 @@
 package medium;
 
 public class Exercicio10 {
-    public static void main(String[] args) {
-        
-        int []vetorFibonacci;
-        vetorFibonacci = new int [10];
 
+    public int calcularFibonacci(int termo) {
         int i = 2;
-     
-        vetorFibonacci[0] = 0;
-        vetorFibonacci[1] = 1;
-     
-        while (i<=9) {
-            vetorFibonacci[i] = vetorFibonacci[i-1] + vetorFibonacci[i-2];
+        int numDigitado = termo;
+        int[] vetFibonacci = new int[numDigitado];
+        vetFibonacci[0] = 0;
+        vetFibonacci[1] = 1;
+        while (i < numDigitado) {
+            vetFibonacci[i] = vetFibonacci[i - 1] + vetFibonacci[i - 2];
             i++;
-     
         }
-         
-         i= 0;
-     
-         while (i<=9) {
-             System.out.println(vetorFibonacci [i]);
-             i++;
-     
-         }
+        return vetFibonacci[termo-1];
 
+    }
 
+    public int[] calcularFibonacciVetor(int termo) {
+        int i = 2;
+        int numDigitado = termo;
+        int[] vetFibonacci = new int[numDigitado];
+        vetFibonacci[0] = 0;
+        vetFibonacci[1] = 1;
+        while (i < numDigitado) {
+            vetFibonacci[i] = vetFibonacci[i - 1] + vetFibonacci[i - 2];
+            i++;
+        }
+        return vetFibonacci;
 
     }
 }
