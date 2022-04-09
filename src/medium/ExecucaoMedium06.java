c20a0b96d6ecd7ae70eac60c47c3a64ccd3a247d
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-04-09 11:49:18 -0300

    ExMedium06 Refatoracao

---
+++ src/medium/ExecucaoMedium06.java
@@ -0,0 +1,10 @@
+package medium;
+
+public class ExecucaoMedium06 {
+    public static void main(String[] args) {
+        
+        Exercicio06 exercicio06 = new Exercicio06(); 
+        System.out.println(exercicio06.calculaFatorial(5));
+
+    }
+}
