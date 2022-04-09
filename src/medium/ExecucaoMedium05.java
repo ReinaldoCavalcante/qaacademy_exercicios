d9e603451c3d19f07b452f9e0259188a26df3b12
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-04-09 10:54:26 -0300

    ExMedium05 Refatoracao

---
+++ src/medium/ExecucaoMedium05.java
@@ -0,0 +1,12 @@
+package medium;
+
+public class ExercicioMedium05 {
+    public static void main(String[] args) {
+        
+        int[] vetorNumeros = { 5, 4, 8, 1, 2 }; 
+        Exercicio05 exercicio05 = new Exercicio05();
+        System.out.println(exercicio05.verificaMenorValor(vetorNumeros));
+
+    }
+    }
+
--- src/medium/Exercicio05.java
+++ src/medium/Exercicio05.java
@@ -1,24 +1,22 @@
 package medium;
 
-import javax.swing.JOptionPane;
 
 public class Exercicio05 {
-    public static void main(String[] args) {
 
-        double i = 1, anterior = 0, numeroDigitado;
-
+    public int verificaMenorValor(int[] vetorNumeros) {
+        int i = 1, menor = 0, numeroDigitado;
         while (i <= 5) {
-            numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
 
-            if (numeroDigitado < anterior || i == 1) {
-                anterior = numeroDigitado;
+
+            numeroDigitado = vetorNumeros[i - 1];
+            if (numeroDigitado < menor || i == 1) {
+                menor = numeroDigitado;
             }
-
             i++;
-
         }
 
-        System.out.println("O menor numero é " + anterior);
-
+        return menor;
     }
+
+
 }
