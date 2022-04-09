ac991ea46f7c6f0a448e61e0f8fb7326a9430e2d
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-04-08 22:58:26 -0300

    ExEasy11 Refatoracao

---
+++ src/easy/ExecucaoEx11.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class ExecucaoEx11 {
+    public static void main(String[] args) {
+        
+        Exercicio11 exercicio11 = new Exercicio11(); 
+        System.out.println(exercicio11.somarDois(10));
+
+        exibirVetor(exercicio11.somarDoisComVetor(10));
+
+
+    }
+
+    private static void exibirVetor(int[] somarDoisComVetor) {
+    }
+}
