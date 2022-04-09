b21a61f39c5780c18ce5cbcd0cf33754ea85378f
Reinaldo Cavalcante <albuquerque.cavalcante01@gmail.com>
2022-04-09 11:51:10 -0300

    ExMedium09 Refatoracao

---
+++ src/medium/ExecucaoMedium09.java
@@ -0,0 +1,12 @@
+package medium;
+
+public class ExecucaoMedium09 {
+    public static void main(String[] args) {
+        
+        String [] vetorAlunos = {"Flavio", "Reinaldo", "Samuel", "Edmond", "Pedro"}; 
+        int [] vetorNumeroAlunos = {1,2,3,4,5}; 
+
+        Exercicio09 exercicio09 = new Exercicio09(); 
+        exercicio09.exibeAlunosENumeros(vetorAlunos,vetorNumeroAlunos);
+    }
+}
--- src/medium/Exercicio09.java
+++ src/medium/Exercicio09.java
@@ -1,31 +1,15 @@
 package medium;
 
-import javax.swing.JOptionPane;
+
 
 public class Exercicio09 {
-    public static void main(String[] args) {
-        
-    String[] vetorAlunos;
-    vetorAlunos = new String [5];
 
-    int [] vetorNumeroAlunos;
-    vetorNumeroAlunos = new int [5];
+    public void exibeAlunosENumeros(String vetorAlunos[], int vetorNumeroAlunos[]) {
+        int i = 0;
 
-    int i = 0;
-
-
-    while (i<=4) {
-        vetorAlunos[i] = JOptionPane.showInputDialog("Digite o nome do Aluno");
-        vetorNumeroAlunos [i] = Integer.parseInt(JOptionPane.showInputDialog ("Numero do Aluno"));
-        i++;
-    }
-
-    i = 0;
-
-    while (i<=4) {
-        System.out.println( vetorAlunos [i]);
-        System.out.println( vetorNumeroAlunos [i]);
-        i++;
+        while (i <= 4) {
+            System.out.println(vetorAlunos[i]);
+            System.out.println(vetorNumeroAlunos[i]);
 
 }
 
